331d47e3e78c0a028048f64df7b7b274661d8457
Simone Faricelli <16625698+Blacksimo@users.noreply.github.com>
2020-01-05 18:34:36 +0100

    updated data gathering from api calls

--- id_web_check.m
+++ id_web_check.m
@@ -1,31 +1,69 @@
 clear
 addpath('dataset')
 load('cleaned_data.mat');
-% api_call = 'http://rest.genenames.org/fetch/symbol/';
-api_call = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&retmode=json&id=';
-
-for i=1:size(geneId,1)
-    fprintf('Analying Gene %i/%i ', i, size(geneId,1));
-    data = webread(strcat(api_call, string(geneId(i, :))));
-    results = struct2cell(data.result);
-    results = results{2,1};
-%     if data.response.numFound ~= 1
+% symbol_max_length = 8;
+api_call = 'http://rest.genenames.org/fetch/symbol/';
+% api_call = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=gene&retmode=json&id=';
+error_name = [];
+error_index = [];
+error_id = [];
+to_check = [];
+uniprot_ids = {};
+protein_name = {};
+geneSymbol_new = {};
+description = {};
+fprintf('-------------------------------------------------------\n');
+for i=1:size(geneSymbol,1)
+    geneSymbol_new = [geneSymbol_new; {strip(geneSymbol(i, :))}];
+    fprintf('Analying Gene %8s %3i/%3i \t', strip(geneSymbol(i,:)),  i, size(geneSymbol,1));
+    data = webread(strcat(api_call, string(geneSymbol(i, :))));
+%     results = struct2cell(data.result);
+%     results = results{2,1};
+    if data.response.numFound ~= 1
 %         fprintf('Error found in checking the gene symbol: %s\n', geneSymbol(i,:));
 %         fprintf('Number of gene found: %i\n', data.response.numFound);
 %         fprintf('Index in geneSymbol array: %i\n', i);
-%     else 
+        fprintf('Error \n');
+        fprintf('-------------------------------------------------------\n');
+        error_id = [error_id geneId(i,:)];        
+        error_index = [error_index i];
+        uniprot_ids = [uniprot_ids; {'null'}];
+        protein_name = [protein_name; {'null'}];
+        description = [description; {'null'}];
+    else 
+        if ~contains(data.response.docs.status, 'Approved') && str2num(data.response.docs.date_modified(1:4))<2010
+            to_check = [to_check geneSymbol(i, :)];
+        end
+        fprintf('\n Status: \t %s \n Last Date: \t %s \n', data.response.docs.status, data.response.docs.date_modified(1:4));
+        fprintf('-------------------------------------------------------\n');
+        uniprot_ids = [uniprot_ids; data.response.docs.uniprot_ids{1}];
+        protein_name = [protein_name; data.response.docs.name];
+        description = [description; data.response.docs.locus_type];
+%         fprintf('Ok \n');
 %         fprintf('Gene ID analyzed: %i\n', geneId(i,:));
 %         fprintf('Gene ID found: %s\n', data.response.docs.hgnc_id);
-%     end
-    if ~contains(geneSymbol(i,:), results.name)
-        fprintf('\n');
-        fprintf('Gene Symbol analyzed: %s\n', geneSymbol(i,:));
-        fprintf('Gene Symbol found: %s\n', results.name);
-    else
-        fprintf('Ok \n');
     end
+%     if ~contains(geneSymbol(i,:), results.name)
+%         fprintf('\n');
+%         fprintf('Gene Symbol analyzed: %s\n', geneSymbol(i,:));
+%         fprintf('Gene Symbol found: %s\n', results.name);
+%     else
+%         fprintf('Ok \n');
+%     end
 
     
 end
+error_name = geneSymbol(error_index, :);
+if size(to_check, 1) == 0
+    fprintf('Every Gene is Approved and More recent than 2010\n');
+    complete_data = table(geneSymbol_new, geneId, uniprot_ids, protein_name, description);
+else
+    fprintf('Some gene needs to be checked (not approved or not up-to-date\n');
+end
 % data = webread('http://rest.genenames.org/fetch/symbol/ACOX1');
 % data_fake = webread('http://rest.genenames.org/fetch/symbol/pippo');
+a = webread('http://www.ebi.ac.uk/proteins/api/proteins/Q15067');
+
+
+
+
