7bd91f49eeef1f2051d9d923a97845ae2eabcf16
Simone Faricelli <16625698+Blacksimo@users.noreply.github.com>
2020-01-15 00:35:07 +0100

    push the button

--- intersection_interactome.m
+++ intersection_interactome.m
@@ -2,11 +2,15 @@ clear
 addpath('dataset')
 load('union_interactome.mat')
 
-intersection = union_interactome;
+counter = 0;
 
 for i=1:size(union_interactome, 1)
-   if (ismember(union_interactome.Database_Source(i), 'IID, BioGrid'))
-      index = intersection.Gene_Symbol_A == union_interactome.Gene_Symbol_A(i);
-      intersection(index) = [];
+   if ismember(union_interactome.Database_Source(i), 'IID, BioGrid')
+       if counter == 0
+           intersection = table(union_interactome.Gene_Symbol_A(i), union_interactome.Gene_Symbol_B(i), union_interactome.Uniprot_A(i), union_interactome.Uniprot_B(i), union_interactome.Database_Source(i));
+          counter = counter + 1; 
+       else
+           intersection = [intersection; table(union_interactome.Gene_Symbol_A(i), union_interactome.Gene_Symbol_B(i), union_interactome.Uniprot_A(i), union_interactome.Uniprot_B(i), union_interactome.Database_Source(i))];
+       end
    end
 end
